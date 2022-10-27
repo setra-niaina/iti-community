475c5deb96627edb9cdc7c5adb0bea2c33b25564
RASOLOMANANA Setraniaina Mianin' Harizo <rasolomanana@et.intechinfo.fr>
2022-10-27 14:25:58 +0200

    register check username

--- src/modules/user/components/user-registration/user-registration.component.ts
+++ src/modules/user/components/user-registration/user-registration.component.ts
@@ -30,13 +30,28 @@ export class UserRegistrationComponent implements OnInit {
 
   async submit() {
 
+    console.log(this.model);
+
     // TODO  Vérifier que la confirmation de mot de passe correspond au mot de passe
     if (this.form.form.invalid || this.model.password !== this.model.confirmPassword) {
       return;
     }
-
     // TODO Enregistrer l'utilisateur via le UserService
+    this.userService.search(this.model.username).then((resp) => {
+      for (let i = 0; i < resp.length; i++) {
+        if (resp[i].username === this.model.username) {
+          console.log("nmom d'utilisateur déjà pris");
+          break;
+        }
+        else {
+          this.userService.register(this.model.username, this.model.password);
+        }
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
     this.goToLogin();
+
   }
 
   goToLogin() {
