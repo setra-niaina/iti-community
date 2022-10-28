658074bd8928da3680dc65b543bb7a9fc2f19ce3
RASOLOMANANA Setraniaina Mianin' Harizo <rasolomanana@et.intechinfo.fr>
2022-10-28 14:49:08 +0200

    affichage post + dev environnement

--- src/modules/notification/notification.module.ts
+++ src/modules/notification/notification.module.ts
@@ -14,7 +14,7 @@ import { NzMessageModule } from 'ng-zorro-antd/message';
   providers: [NotificationStore, NotificationService,
     {
       provide: NotificationQueries,
-      useClass: LocalNotificationQueries
+      useClass: HttpNotificationQueries
     }, {
       provide: NotificationCommands,
       useClass: HttpNotificationCommands
